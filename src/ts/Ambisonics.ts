b95e93c6b47028b182ee9d2bba4de2bdd635a470
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2017-09-13 14:26:47 +0200

    Fix typo

--- src/ts/Ambisonics.ts
+++ src/ts/Ambisonics.ts
@@ -93,7 +93,7 @@ export class Ambisonics {
 
       this.foaRenderer = Omnitone.createFOARenderer(audioContext, {
         HRIRUrl: 'https://cdn.rawgit.com/GoogleChrome/omnitone/962089ca/build/resources/sh_hrir_o_1.wav',
-        // Remap channels from FuMa ordering (W,X,Y,Z) ACN
+        // Remap channels from FuMa ordering (W,X,Y,Z) to ACN
         channelMap: [0, 3, 1, 2],
       });
 
